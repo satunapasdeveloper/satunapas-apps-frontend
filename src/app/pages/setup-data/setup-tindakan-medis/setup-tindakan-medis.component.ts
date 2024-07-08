366b556762d6843e66142419d56509b0e07d1d68
Fatur Gautama S <faturgautama.dev@gmail.com>
2024-07-08 09:24:36 +0700

    fix: remove gtm script

---
+++ src/app/pages/setup-data/setup-tindakan-medis/setup-tindakan-medis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setup-tindakan-medis',
+  standalone: true,
+  imports: [],
+  templateUrl: './setup-tindakan-medis.component.html',
+  styleUrl: './setup-tindakan-medis.component.scss'
+})
+export class SetupTindakanMedisComponent {
+
+}
