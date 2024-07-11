a1ac7a161de7325810c95f875de27cb64880faf8
Fatur Gautama S <faturgautama.dev@gmail.com>
2024-07-11 17:21:45 +0700

    feat: diagnosa ui

--- src/app/pages/rekam-medis/input-rekam-medis/diagnosis/diagnosis.component.ts
+++ src/app/pages/rekam-medis/input-rekam-medis/diagnosis/diagnosis.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
-  selector: 'app-diagnosis',
-  standalone: true,
-  imports: [],
-  templateUrl: './diagnosis.component.html',
-  styleUrl: './diagnosis.component.scss'
+    selector: 'app-diagnosis',
+    standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule,
+        DropdownModule,
+        ButtonModule
+    ],
+    templateUrl: './diagnosis.component.html',
+    styleUrl: './diagnosis.component.scss'
 })
-export class DiagnosisComponent {
+export class DiagnosisComponent implements OnInit {
 
+    DiagnosaDatasource: any[] = [];
+
+    JenisDiagnosa: any[] = [
+        { label: 'Diagnosa Utama / Primer', value: 'Diagnosa Utama / Primer' },
+        { label: 'Diagnosa Tambahan / Sekunder', value: 'Diagnosa Tambahan / Sekunder' },
+    ];
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.handleAddDiagnosa();
+    }
+
+    handleAddDiagnosa() {
+        this.DiagnosaDatasource.push({
+            id: 0,
+            icd10: '',
+            jenis: '',
+        })
+    };
+
+    handleDeleteDiagnosa(index: number) {
+        if (this.DiagnosaDatasource.length > 0) {
+            this.DiagnosaDatasource.splice(index, 1);
+        }
+    }
 }
