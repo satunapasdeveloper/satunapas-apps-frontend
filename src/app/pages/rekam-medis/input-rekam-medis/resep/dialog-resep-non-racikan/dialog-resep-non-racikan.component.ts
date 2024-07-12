9369ee8d3cdbc21043888ad446a48e0068a37780
Fatur Gautama S <faturgautama.dev@gmail.com>
2024-07-12 16:37:53 +0700

    feat: resep non racikan ui

--- src/app/pages/rekam-medis/input-rekam-medis/resep/dialog-resep-non-racikan/dialog-resep-non-racikan.component.ts
+++ src/app/pages/rekam-medis/input-rekam-medis/resep/dialog-resep-non-racikan/dialog-resep-non-racikan.component.ts
@@ -1,12 +1,126 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { DynamicFormComponent } from 'src/app/components/form/dynamic-form/dynamic-form.component';
+import { FormModel } from 'src/app/model/components/form.model';
+import { RekamMedisService } from 'src/app/services/rekam-medis/rekam-medis.service';
 
 @Component({
-  selector: 'app-dialog-resep-non-racikan',
-  standalone: true,
-  imports: [],
-  templateUrl: './dialog-resep-non-racikan.component.html',
-  styleUrl: './dialog-resep-non-racikan.component.scss'
+    selector: 'app-dialog-resep-non-racikan',
+    standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        DialogModule,
+        DropdownModule,
+        InputTextModule,
+        InputNumberModule,
+        CheckboxModule,
+        ButtonModule,
+    ],
+    templateUrl: './dialog-resep-non-racikan.component.html',
+    styleUrl: './dialog-resep-non-racikan.component.scss'
 })
-export class DialogResepNonRacikanComponent {
+export class DialogResepNonRacikanComponent implements OnInit {
 
+    ShowDialog = false;
+
+    ObatDatasource: any[] = [
+        {
+            id: 1,
+            nama_obat: 'Paracetamol 250 mg Sirup (OBAPA) x 1',
+            harga: 12000
+        }
+    ];
+
+    SelectedObat: any;
+
+    WaktuPemberianObat: any[] = this._rekamMedisService.WaktuPemberianObat;
+
+    WaktuSpesifikPemberianObat: any[] = this._rekamMedisService.WaktuSpesifikPemberianObat;
+
+    RutePemberianObat: any[] = this._rekamMedisService.RutePemberianObat;
+
+    FormState: 'insert' | 'update' = 'insert';
+
+    FormResep: FormGroup;
+
+    @Output('onSave') onSave = new EventEmitter<any>();
+
+    ObatEditedIndex: number = 0;
+
+    constructor(
+        private _formBuilder: FormBuilder,
+        private _rekamMedisService: RekamMedisService
+    ) {
+        this.FormResep = this._formBuilder.group({
+            nama_obat: ['', []],
+            qty: [0, []],
+            total: [0, []],
+            aturan_pakai: ["", []],
+            waktu_pemberian_obat: [[], []],
+            waktu_spesifik_pemberian_obat: [[], []],
+            rute_pemberian_obat: ["", []],
+        });
+    }
+
+    ngOnInit(): void {
+
+    }
+
+    handleOpenDialog(state: 'insert' | 'update', index?: number, data?: any) {
+        this.FormState = state;
+
+        if (state == 'update') {
+            this.FormResep.setValue(data);
+            this.ObatEditedIndex = index!;
+        } else {
+            this.FormResep.reset();
+            this.ObatEditedIndex = 0;
+            this.FormResep = this._formBuilder.group({
+                nama_obat: ['', []],
+                qty: [0, []],
+                total: [0, []],
+                aturan_pakai: ["", []],
+                waktu_pemberian_obat: [[], []],
+                waktu_spesifik_pemberian_obat: [[], []],
+                rute_pemberian_obat: ["", []],
+            });
+        }
+
+        this.ShowDialog = true;
+    }
+
+    handleCloseDialog() {
+        this.ShowDialog = false;
+    }
+
+    handleChangeObat(args: any) {
+        this.SelectedObat = args.value;
+        this.FormResep.get('qty')?.setValue(1);
+        this.FormResep.get('total')?.setValue(args.value.harga);
+        this.FormResep.get('nama_obat')?.setValue(args.value.nama_obat);
+    }
+
+    handleChangeQty(args: any) {
+        const qty = this.FormResep.get('qty')?.value;
+
+        if (qty > 1) {
+            this.FormResep.get('total')?.setValue(qty * this.SelectedObat.harga);
+        }
+    }
+
+    handleSave(data: any) {
+        if (this.FormResep.valid) {
+            this.onSave.emit({ state: this.FormState, index: this.ObatEditedIndex, data: data });
+            this.handleCloseDialog();
+        }
+    }
 }
--- src/app/pages/rekam-medis/input-rekam-medis/resep/resep.component.ts
+++ src/app/pages/rekam-medis/input-rekam-medis/resep/resep.component.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { DialogResepNonRacikanComponent } from './dialog-resep-non-racikan/dialog-resep-non-racikan.component';
+import { DialogResepRacikanComponent } from './dialog-resep-racikan/dialog-resep-racikan.component';
+import { DialogResepManualComponent } from './dialog-resep-manual/dialog-resep-manual.component';
 
 @Component({
     selector: 'app-resep',
     standalone: true,
     imports: [
         CommonModule,
+        ButtonModule,
+        DialogResepNonRacikanComponent,
+        DialogResepRacikanComponent,
+        DialogResepManualComponent,
         ButtonModule,
     ],
     templateUrl: './resep.component.html',
@@ -16,11 +23,35 @@ export class ResepComponent implements OnInit {
 
     ResepNonRacikan: any[] = [];
 
+    @ViewChild('DialogResepNonRacikanComps') DialogResepNonRacikanComps!: DialogResepNonRacikanComponent;
+
     ResepRacikan: any[] = [];
 
     ResepManual: any[] = [];
 
     ngOnInit(): void {
 
+    }
+
+    handleOpenDialogResepNonRacikan(state: 'insert' | 'update') {
+        this.DialogResepNonRacikanComps.handleOpenDialog(state);
+    }
+
+    onFormatAturanPakai(data: any) {
+        const aturan_pakai = data.aturan_pakai;
+
+        const waktu_spesifik_pemberian_obat = data.waktu_spesifik_pemberian_obat.join(',');
+
+        const waktu_pemberian_obat = data.waktu_pemberian_obat.join(',');
+
+        return `${aturan_pakai} | ${waktu_spesifik_pemberian_obat} | ${waktu_pemberian_obat}`;
+    }
+
+    handleSaveObatNonRacikan(args: any) {
+        if (args.state == 'insert') {
+            this.ResepNonRacikan.push(args.data);
+        } else {
+            this.ResepNonRacikan[args.index] = args.data;
+        }
     }
 }
