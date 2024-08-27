70c89f176ad2dbc60253ad02efc2d818ce7586f4
Fatur Gautama S <faturgautama.dev@gmail.com>
2024-08-27 15:11:08 +0700

    feat: dokumen resume medis

--- src/app/pages/layanan-dokumen/resume-medis/resume-medis.component.ts
+++ src/app/pages/layanan-dokumen/resume-medis/resume-medis.component.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormPilihPasienForDokumenComponent } from 'src/app/components/form/form-pilih-pasien-for-dokumen/form-pilih-pasien-for-dokumen.component';
 import { DashboardComponent } from 'src/app/components/layout/dashboard/dashboard.component';
 import { LayoutModel } from 'src/app/model/components/layout.model';
+import { RiwayatRekamMedisComponent } from '../../rekam-medis/riwayat-rekam-medis/riwayat-rekam-medis.component';
+import { Store } from '@ngxs/store';
+import { RekamMedisActions } from 'src/app/store/rekam-medis';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'app-resume-medis',
@@ -10,12 +14,15 @@ import { LayoutModel } from 'src/app/model/components/layout.model';
     imports: [
         CommonModule,
         DashboardComponent,
-        FormPilihPasienForDokumenComponent
+        FormPilihPasienForDokumenComponent,
+        RiwayatRekamMedisComponent,
     ],
     templateUrl: './resume-medis.component.html',
     styleUrl: './resume-medis.component.scss'
 })
-export class ResumeMedisComponent implements OnInit {
+export class ResumeMedisComponent implements OnInit, OnDestroy {
+
+    Destroy$ = new Subject();
 
     ButtonNavigation: LayoutModel.IButtonNavigation[] = [
         {
@@ -25,13 +32,33 @@ export class ResumeMedisComponent implements OnInit {
         }
     ];
 
-    constructor() { }
+    ShowRiwayatRekamMedis = false;
+
+    constructor(
+        private _store: Store,
+    ) { }
 
     ngOnInit(): void {
 
     }
 
+    ngOnDestroy(): void {
+        this.Destroy$.next(0);
+        this.Destroy$.complete();
+    }
+
     handleClickButtonNavigation(args: any) {
         console.log(args);
+    }
+
+    handleChooseRiwayatKunjungan(args: any) {
+        this._store
+            .dispatch(new RekamMedisActions.GetResumeMedis(args.id_pendaftaran))
+            .pipe(takeUntil(this.Destroy$))
+            .subscribe((result) => {
+                if (result.rekam_medis.success) {
+                    this.ShowRiwayatRekamMedis = true;
+                }
+            });
     }
 }
